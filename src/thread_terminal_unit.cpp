bbe2abd760a9cc4853ed34d7d49d9b83dd2ec82b
PedroHRPBS <pedro.silva@ku.ac.ae>
2020-01-13 09:23:52 +0400

    remove FixedQueue

--- src/thread_terminal_unit.cpp
+++ src/thread_terminal_unit.cpp
@@ -3,23 +3,18 @@ using namespace std;
 
 void thread_terminal_unit::receive_msg_data(DataMessage* t_msg){
     this->lock_mutex();
-    synced_message[id].push(t_msg->Clone());
+    synced_message[id]=t_msg->Clone();
     this->unlock_mutex();
 }
 
 DataMessage* thread_terminal_unit::clone_last_message(){
-    synced_message[id].pop();
-    if(synced_message[id].size() < 1){ //This is to handle the beggining of the code. It doesn't happen later.
-        synced_message[id].push(new XSensMessage);
-        std::cout << "WARNING" << std::endl;
-    }
-    return synced_message[id].front();
+    return synced_message[id]->Clone();
 }
 
 thread_terminal_unit::thread_terminal_unit(){
     id=thread_terminal_counter;
     thread_terminal_counter++;
-    synced_message[id].push(new XSensMessage());//This is dummy to prevent possible segmentation fault at first read
+    synced_message[id] = new XSensMessage();//This is dummy to prevent possible segmentation fault at first read
 }
 
 void thread_terminal_unit::lock_mutex(){

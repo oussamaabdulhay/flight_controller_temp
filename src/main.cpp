6a837207bdc32240f899cfb3a21e14e5fadd0198
PedroHRPBS <pedro.silva@ku.ac.ae>
2020-03-11 16:51:15 +0400

    Changes on MAIN.CPP. For future consultation on main, checkout this commit.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 #include <vector>
 #include "ROSUnit_Optitrack.hpp"
-#include "PIDController.hpp"
-#include "ControlSystem.hpp"
-#include "PID_values.hpp"
-#include "ProcessVariableReference.hpp"
-#include "ActuationSystem.hpp"
 #include "std_logger.hpp"
 #include "HexaActuationSystem.hpp"
 #include "ESCMotor.hpp"
@@ -15,14 +10,8 @@
 #include "ROSUnit_BroadcastData.hpp"
 #include "ROSUnit_SwitchBlock.hpp"
 #include "MRFTController.hpp"
-#include "MRFT_values.hpp"
-#include "ControllerMessage.hpp"
 #include "ROSUnit_Xsens.hpp"
-#include "XSens_IMU.hpp"
 #include "Transform_InertialToBody.hpp"
-#include <iostream>
-#include <stdexcept>
-#include <string>
 #include "CallbackHandler.hpp"
 #include "RestrictedNormWaypointRefGenerator.hpp"
 #include "ROSUnit_RestNormSettings.hpp"
@@ -34,20 +23,9 @@
 #include "ROSUnit_RTK.hpp"
 #include "Differentiator.hpp"
 #include "PVConcatenator.hpp"
-#include <thread>
-#include "xsstatusflag.h"
 #include "WrapAroundFunction.hpp"
-#include "IntegerMsg.hpp"
-#include "ROSUnit_Xsens.hpp"
 
-#define DEBUG_HR_LR_DECOUPLED
 #define XSENS_OVER_ROS
-#undef RTK_GPS_FILTER
-#undef XSENS_OVER_SERIAL
-#undef RTK_ONLY
-#undef XSENS_GPS_ONLY
-
-#undef Navio_IMU_en
 #define OPTITRACK
 #undef BATTERY_MONITOR
 
@@ -56,39 +34,11 @@ const int PWM_FREQUENCY = 50;
 const float SATURATION_VALUE_XY = 0.5;
 const float SATURATION_VALUE_YAWRATE = 1.0;
 
-ROSUnit* myROSBroadcastData;
-MsgEmitter error_emitter;
-
 Journaller *gJournal = 0;
-
-#ifdef RTK_GPS_FILTER
-void worker(TimedBlock* timed_block) {
-    timed_block->tickTimer();
-    usleep(timed_block->getLoopRemainingMicroSec());
-    while(true){
-        while(!timed_block->hasLoopTimeElapsed()){
-        }
-        timed_block->tickTimer();
-        if(timed_block->getLoopRemainingMicroSec() < 0){
-            Logger::getAssignedLogger()->log("exceeded loop time 100hz ",LoggerLevel::Warning);
-            
-            IntegerMsg error_msg; //TODO make enum of different errors
-            error_msg.data = 1;
-            error_emitter.emitMsgUnicastDefault((DataMessage*)&error_msg);
-            
-        } else {
-            usleep(timed_block->getLoopRemainingMicroSec());
-        }
-    }
-}
-#endif
 
 int main(int argc, char** argv) {
     
-    std::cout << "Hello Easy C++ project!" << std::endl;
-    //TODO separate files on specific folders
-    //TODO make a single pattern to follow for providers
-    //TODO makedirect connection between Xsens and Raspberry
+    std::cout << "Hello Flight Controller!" << std::endl;
 
     //*****************************ROS UNITS*******************************
 
@@ -112,166 +62,7 @@ int main(int argc, char** argv) {
     //*****************************LOGGER**********************************
     Logger::assignLogger(new StdLogger());
     //***********************ADDING SENSORS********************************
-    #ifdef BATTERY_MONITOR
-    BatteryMonitor* myBatteryMonitor = new BatteryMonitor();
-    #endif
-    #ifdef Navio_IMU_en
-    NAVIOMPU9250_sensor* myIMU = new NAVIOMPU9250_sensor();
-    myIMU->setSettings(ACCELEROMETER, FSR, 16);
-    myIMU->setSettings(GYROSCOPE, FSR, 2000);
-    myIMU->setSettings(MAGNETOMETER, FSR, 16);
-    #endif
     
-    
-    #ifdef XSENS_OVER_SERIAL
-    
-    //For the XSens to wokr over USB/FTDI connection, it's necessary to install a program called "setserial" on Linux
-    //Once installed, run the command "setserial /name/of/the/dev/port low_latency". Doing so, will reduce the buffering
-    //from the the FTDI driver, and the data will arrive at almost constant rates.
-    //https://base.xsens.com/hc/en-us/community/posts/360029341694-How-do-I-get-IMU-data-via-USB-in-real-time-
-
-    cout << "Creating XsControl object..." << endl;
-	XsControl* control = XsControl::construct();
-	assert(control != 0);
-
-	// Lambda function for error handling
-	auto handleError = [=](string errorString)
-	{
-		control->destruct();
-		cout << errorString << endl;
-		cout << "Press [ENTER] to continue." << endl;
-		cin.get();
-		return -1;
-	};
-
-	cout << "Scanning for devices..." << endl;
-	//XsPortInfoArray portInfoArray = XsScanner::scanPorts(XsBaudRate::XBR_460k8,100,false);
-	// Find an MTi device
-	XsPortInfo mtPort;
-    
-    XsString portName("/dev/ttyAMA0");
-    cout << "Input: " << portName.toStdString() << " @ baud: " <<(int) XsBaudRate::XBR_460k8 << endl;
-
-    XsPortInfo ManualmtPort=XsScanner::scanPort(portName, XsBaudRate::XBR_460k8);
-    
-    cout << "List: " << ManualmtPort.deviceId().toString().toStdString() << " @ port: " << ManualmtPort.portName().toStdString() << ", baudrate: " << ManualmtPort.baudrate() << endl;
-    
-    cout << "isMti: " << ManualmtPort.deviceId().isMti() << " @ MtiG: " << ManualmtPort.deviceId().isMtig() << endl;
-
-    XsPortInfoArray portInfoArray; //TODO: Remove
-    for (auto const &portInfo : portInfoArray)
-	{
-		if (portInfo.deviceId().isMti() || portInfo.deviceId().isMtig())
-		{
-			mtPort = portInfo;
-			break;
-		}
-	    cout << "List: " << portInfo.deviceId().toString().toStdString() << " @ port: " << portInfo.portName().toStdString() << ", baudrate: " << portInfo.baudrate() << endl;
-	}
-
-	if (ManualmtPort.empty())
-		return handleError("No MTi device found. Aborting.");
-
-	cout << "Found a device with ID: " << ManualmtPort.deviceId().toString().toStdString() << " @ port: " << ManualmtPort.portName().toStdString() << ", baudrate: " << ManualmtPort.baudrate() << endl;
-
-	cout << "Opening port..." << endl;
-    if (!control->openPort(ManualmtPort.portName().toStdString(), ManualmtPort.baudrate())){//Timeout in ms
-        return handleError("Could not open port. Aborting.");
-    }
-
-
-	// Get the device object
-
-	XsDevice* device = control->device(ManualmtPort.deviceId());
-	assert(device != 0);
-	cout << "Device: " << device->productCode().toStdString() << ", with ID: " << device->deviceId().toString() << " opened." << endl;
-
-	// Create and attach callbackXSens handler to device
-	CallbackHandler callbackXSens;
-	device->addCallbackHandler(&callbackXSens);
-
-	cout << "Putting device into measurement mode..." << endl;
-	if (!device->gotoMeasurement())
-		return handleError("Could not put device into measurement mode. Aborting.");
-
-    char ans;
-    uint16_t ans_time;
-    std::cout << "Do gyro calibration? (y/n)" << std::endl;
-    std::cin >> ans;
-    if(ans == 'y'){
-        //************* GYRO CALIBRATION PROCEDURE *****************
-        std::cout << "For how long? (recommended: 6) (s)" << std::endl;
-        std::cin >> ans_time;
-        XsTime::msleep(100); //Give the device a little bit of time to enter measurement mode
-        uint16_t biasEstimateDuration = ans_time;
-        device->setNoRotation(biasEstimateDuration);
-        bool noRotationStarted = false;
-        int64_t biasEstimateStart = XsTime::timeStampNow();
-        while (true){
-            if (callbackXSens.packetAvailable()){
-                    XsDataPacket packet = callbackXSens.getNextPacket();
-                    uint32_t noRotationStatus = packet.status() & XSF_NoRotationMask;
-
-                if (!noRotationStarted){
-                    if (noRotationStatus == XSF_NoRotationRunningNormally){
-                        std::cout << "No rotation started"<< std::endl;
-                        noRotationStarted = true;
-                    }
-                }
-                else{
-                    if (noRotationStatus == 0){
-                        std::cout << "No rotation ended successfully"<< std::endl;
-                        break;
-                    }
-                }
-
-
-                if (noRotationStatus == XSF_NoRotationAborted){
-                    std::cout << "No rotation aborted" << std::endl;
-                    break;
-                }
-
-                if (noRotationStatus == XSF_NoRotationSamplesRejected){
-                    std::cout << "No rotation ended with rejected samples" << std::endl;
-                    break;
-                }
-            }
-
-            //Check for timeout
-            int64_t timeout = ((int64_t)biasEstimateDuration * 1000 + 500);
-            if ( (XsTime::timeStampNow() - biasEstimateStart) > timeout)
-            {
-            if (!noRotationStarted)
-            std::cout << "No rotation did not start in time" << std::endl;
-            else
-            std::cout << "No rotation did not end in time" << std::endl;
-            break;
-            }
-            XsTime::msleep(0);
-        }
-        //************* END OF GYRO CALIBRATION PROCEDURE *****************
-    }
-    
-    XSens_IMU* myXSensIMU = new XSens_IMU();
-
-    #endif
-
-    
-
-    //***********************FILTER RTK_GPS*************************************
-    #ifdef RTK_GPS_FILTER
-    HR_LR_position_fusion* hr_lr_position_fusion = new HR_LR_position_fusion();
-    hr_lr_position_fusion->setLoopFrequency(block_frequency::hz100);
-    hr_lr_position_fusion->current_operation_mode = HR_LR_position_fusion::operation_mode::bias_elimination;
-
-    thread_terminal_unit rtk_position_terminal_unit;
-    thread_terminal_unit xsens_position_terminal_unit;
-
-    rtk_position_terminal_unit.setTerminalUnitAddress(thread_terminal_unit::RTK_pos);
-    xsens_position_terminal_unit.setTerminalUnitAddress(thread_terminal_unit::XSens_pos);
-    ((thread_initial_unit*)hr_lr_position_fusion)->addTerminalUnit(&rtk_position_terminal_unit);
-    ((thread_initial_unit*)hr_lr_position_fusion)->addTerminalUnit(&xsens_position_terminal_unit);
-    #endif
     //***********************SETTING PROVIDERS**********************************
     
     Global2Inertial* myGlobal2Inertial = new Global2Inertial();
@@ -306,14 +97,6 @@ int main(int argc, char** argv) {
     myGlobal2Inertial->addCallbackMsgReceiver((MsgReceiver*)CsYaw_PVConcatenator, (int)Global2Inertial::unicast_addresses::uni_Optitrack_heading);
     #endif
 
-    #ifdef XSENS_OVER_SERIAL
-    callbackXSens.addCallbackMsgReceiver((MsgReceiver*)CsRoll_PVConcatenator,(int)CallbackHandler::unicast_addresses::unicast_XSens_attitude_rate);
-    callbackXSens.addCallbackMsgReceiver((MsgReceiver*)CsPitch_PVConcatenator,(int)CallbackHandler::unicast_addresses::unicast_XSens_attitude_rate);
-    callbackXSens.addCallbackMsgReceiver((MsgReceiver*)CsYawRate_PVConcatenator,(int)CallbackHandler::unicast_addresses::unicast_XSens_yaw_rate);
-    callbackXSens.addCallbackMsgReceiver((MsgReceiver*)myGlobal2Inertial,(int)CallbackHandler::unicast_addresses::unicast_XSens_orientation);
-    callbackXSens.addCallbackMsgReceiver((MsgReceiver*)myGlobal2Inertial,(int)CallbackHandler::unicast_addresses::unicast_XSens_translation);
-    callbackXSens.addCallbackMsgReceiver((MsgReceiver*)myGlobal2Inertial,(int)CallbackHandler::unicast_addresses::unicast_XSens_translation_rate);
-    #endif
     #ifdef XSENS_OVER_ROS
     ROSUnit* myROSUnit_Xsens = new ROSUnit_Xsens(nh);
     myROSUnit_Xsens->addCallbackMsgReceiver((MsgReceiver*)CsRoll_PVConcatenator,(int)ROSUnit_Xsens::unicast_addresses::unicast_XSens_attitude_rate);
@@ -331,45 +114,6 @@ int main(int argc, char** argv) {
     wrap_around_yaw->addCallbackMsgReceiver((MsgReceiver*)CsYaw_PVConcatenator);
     myGlobal2Inertial->addCallbackMsgReceiver((MsgReceiver*)CsRoll_PVConcatenator,(int)Global2Inertial::unicast_addresses::uni_XSens_ori); //TODO bad grouping with ifdef
     myGlobal2Inertial->addCallbackMsgReceiver((MsgReceiver*)CsPitch_PVConcatenator,(int)Global2Inertial::unicast_addresses::uni_XSens_ori);
-
-    #ifdef RTK_GPS_FILTER
-    myGlobal2Inertial->addCallbackMsgReceiver((MsgReceiver*)&rtk_position_terminal_unit, (int)Global2Inertial::unicast_addresses::uni_RTK_pos_pv);
-    myGlobal2Inertial->addCallbackMsgReceiver((MsgReceiver*)&xsens_position_terminal_unit, (int)Global2Inertial::unicast_addresses::uni_XSens_pos);
-    hr_lr_position_fusion->addCallbackMsgReceiver((MsgReceiver*)CsX_PVConcatenator,HR_LR_position_fusion::uni_pv_receiver);
-    hr_lr_position_fusion->addCallbackMsgReceiver((MsgReceiver*)CsY_PVConcatenator,HR_LR_position_fusion::uni_pv_receiver);
-    hr_lr_position_fusion->addCallbackMsgReceiver((MsgReceiver*)CsZ_PVConcatenator,HR_LR_position_fusion::uni_pv_receiver);
-    thread* hr_lr_position_fusion_thread = new thread(worker, (TimedBlock*)hr_lr_position_fusion);
-    #endif
-    #ifdef RTK_ONLY
-    myROSRTK->addCallbackMsgReceiver((MsgReceiver*)myGlobal2Inertial);
-    myGlobal2Inertial->addCallbackMsgReceiver((MsgReceiver*)CsX_PVConcatenator, (int)Global2Inertial::unicast_addresses::uni_RTK_pos_pv);
-    myGlobal2Inertial->addCallbackMsgReceiver((MsgReceiver*)CsY_PVConcatenator, (int)Global2Inertial::unicast_addresses::uni_RTK_pos_pv);
-    myGlobal2Inertial->addCallbackMsgReceiver((MsgReceiver*)CsZ_PVConcatenator, (int)Global2Inertial::unicast_addresses::uni_RTK_pos_pv);
-    #endif
-    #ifdef XSENS_GPS_ONLY
-    myGlobal2Inertial->addCallbackMsgReceiver((MsgReceiver*)CsX_PVConcatenator, (int)Global2Inertial::unicast_addresses::uni_XSens_pos);
-    myGlobal2Inertial->addCallbackMsgReceiver((MsgReceiver*)CsY_PVConcatenator,(int) Global2Inertial::unicast_addresses::uni_XSens_pos);
-    myGlobal2Inertial->addCallbackMsgReceiver((MsgReceiver*)CsZ_PVConcatenator, (int)Global2Inertial::unicast_addresses::uni_XSens_pos);
-    #endif
-
-    
-    #ifdef Navio_IMU_en
-    AccGyroAttitudeObserver myAttObserver((BodyAccProvider*) myIMU->getAcc(), 
-                                          (BodyRateProvider*) myIMU->getGyro(),
-                                          block_frequency::hhz200);
-
-    
-    
-     ComplementaryFilter filter1, filter2, filter3;
-    //TODO second argument should be dt of IMU sampling rate
-     ComplementaryFilterSettings settings(false, 0.005, 0.995);
-
-     myAttObserver.setFilterType(&filter1, &filter2);
-     myAttObserver.updateSettings(&settings, 0.1);
-
-     Roll_PVProvider* myRollPV = (Roll_PVProvider*) &myAttObserver;
-     Pitch_PVProvider* myPitchPV = (Pitch_PVProvider*) &myAttObserver;
-    #endif        
 
     //**************************SETTING BLOCKS**********************************
 
@@ -396,8 +140,6 @@ int main(int argc, char** argv) {
     Block* MRFT_yaw = new MRFTController(block_id::MRFT_YAW);
     Block* MRFT_yaw_rate = new MRFTController(block_id::MRFT_YAW_RATE);
 
-    //TODO make all of these Blocks
-    // Vector3D<float>* inertial_command = new Vector3D<float>(); //TODO-Chehadeh
     Transform_InertialToBody* transform_X_InertialToBody = new Transform_InertialToBody(control_system::x);
     Transform_InertialToBody* transform_Y_InertialToBody = new Transform_InertialToBody(control_system::y);
 
@@ -408,7 +150,7 @@ int main(int argc, char** argv) {
     Saturation* YawRate_Saturation = new Saturation(SATURATION_VALUE_YAWRATE);
 
     //***********************SETTING CONTROL SYSTEMS***************************
-    //TODO Expose switcher to the main, add blocks to the switcher, then make connections between switcher, then add them to the Control System
+
     ControlSystem* X_ControlSystem = new ControlSystem(control_system::x, block_frequency::hz100);
     X_ControlSystem->addBlock(PID_x);
     X_ControlSystem->addBlock(MRFT_x);
@@ -544,8 +286,6 @@ int main(int argc, char** argv) {
     myROSArm->addCallbackMsgReceiver((MsgReceiver*) myActuationSystem);
     #ifdef OPTITRACK
     myGlobal2Inertial->addCallbackMsgReceiver((MsgReceiver*)myWaypoint, (int)Global2Inertial::unicast_addresses::uni_Optitrack_pos); 
-    #else
-    myGlobal2Inertial->addCallbackMsgReceiver((MsgReceiver*)myWaypoint, (int)Global2Inertial::unicast_addresses::uni_RTK_pos_wp); 
     #endif
     ROSUnit_uav_control_set_path->addCallbackMsgReceiver((MsgReceiver*)myWaypoint);
     myROSRestNormSettings->addCallbackMsgReceiver((MsgReceiver*)myWaypoint);
@@ -583,6 +323,7 @@ int main(int argc, char** argv) {
     #ifdef BATTERY_MONITOR
     myBatteryMonitor->addCallbackMsgReceiver((MsgReceiver*)myROSBroadcastData);
     #endif
+    MsgEmitter error_emitter;
     error_emitter.addCallbackMsgReceiver((MsgReceiver*)myROSBroadcastData);
     //***********************INERTIAL TO BODY PROVIDER*****************************
  
@@ -677,15 +418,6 @@ int main(int argc, char** argv) {
         rate.sleep();
     }
 
-    #ifdef XSENS_OVER_SERIAL
-	cout << "Closing port..." << endl;
-	control->closePort(ManualmtPort.portName().toStdString());
-
-	cout << "Freeing XsControl object..." << endl;
-	control->destruct();
-
-	cout << "Successful exit." << endl;
-    #endif
     return 0;
 
 }

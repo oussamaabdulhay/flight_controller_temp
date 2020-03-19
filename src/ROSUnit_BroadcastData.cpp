5fb65ffb8a770a6d3960f4f8573fce3689216cab
PedroHRPBS <pedro.silva@ku.ac.ae>
2020-03-19 15:30:42 +0400

    Changes in ROSBroadcast.

--- src/ROSUnit_BroadcastData.cpp
+++ src/ROSUnit_BroadcastData.cpp
@@ -5,13 +5,6 @@ ROSUnit_BroadcastData::ROSUnit_BroadcastData(ros::NodeHandle& t_main_handler) : 
 
     _pos_prov_pub = t_main_handler.advertise<geometry_msgs::Point>("uav_control/uav_position", 2);
     _ori_prov_pub = t_main_handler.advertise<geometry_msgs::Point>("uav_control/uav_orientation", 2);
-    _xpv_prov_pub = t_main_handler.advertise<geometry_msgs::PointStamped>("x_provider", 2);
-    _ypv_prov_pub = t_main_handler.advertise<geometry_msgs::PointStamped>("y_provider", 2);
-    _zpv_prov_pub = t_main_handler.advertise<geometry_msgs::PointStamped>("z_provider", 2);
-    _rollpv_prov_pub = t_main_handler.advertise<geometry_msgs::PointStamped>("roll_provider", 2);
-    _pitchpv_prov_pub = t_main_handler.advertise<geometry_msgs::PointStamped>("pitch_provider", 2);
-    _yawpv_prov_pub = t_main_handler.advertise<geometry_msgs::PointStamped>("yaw_provider", 2);
-    _yawratepv_prov_pub = t_main_handler.advertise<geometry_msgs::PointStamped>("yaw_rate_provider", 2);
     _cs_prov_pub = t_main_handler.advertise<std_msgs::Float64MultiArray>("control_system_output", 2);
     _csr_prov_pub = t_main_handler.advertise<std_msgs::Float64MultiArray>("control_system_reference", 2);
     _act_prov_pub = t_main_handler.advertise<std_msgs::Float64MultiArray>("actuation_output", 2);
@@ -44,89 +37,32 @@ void ROSUnit_BroadcastData::receiveMsgData(DataMessage* t_msg, int t_channel){
             Vector3D<float> xpv = vector3d_msg->getData();
             _position.x = xpv.x;
             x_received = true;
-            geometry_msgs::PointStamped msg;
-            msg.header.seq = ++_seq_xpv;
-            msg.header.stamp = ros::Time::now();
-            msg.header.frame_id = "";
-            msg.point.x = xpv.x;
-            msg.point.y = xpv.y;
-            msg.point.z = xpv.z;
-            _xpv_prov_pub.publish(msg);
 
         }else if(t_channel == (int)ros_broadcast_channels::y){
             Vector3D<float> ypv = vector3d_msg->getData();
             _position.y = ypv.x;
             y_received = true;
-            geometry_msgs::PointStamped msg;
-            msg.header.seq = ++_seq_ypv;
-            msg.header.stamp = ros::Time::now();
-            msg.header.frame_id = "";
-            msg.point.x = ypv.x;
-            msg.point.y = ypv.y;
-            msg.point.z = ypv.z;
-            _ypv_prov_pub.publish(msg);
 
         }else if(t_channel == (int)ros_broadcast_channels::z){
             Vector3D<float> zpv = vector3d_msg->getData();
             _position.z = zpv.x;
             z_received = true;
-            geometry_msgs::PointStamped msg;
-            msg.header.seq = ++_seq_zpv;
-            msg.header.stamp = ros::Time::now();
-            msg.header.frame_id = "";
-            msg.point.x = zpv.x;
-            msg.point.y = zpv.y;
-            msg.point.z = zpv.z;
-            _zpv_prov_pub.publish(msg);
 
         }else if(t_channel == (int)ros_broadcast_channels::yaw){
             Vector3D<float> yawpv = vector3d_msg->getData();
             _head = yawpv.x;
             yaw_received = true;
-            geometry_msgs::PointStamped msg;
-            msg.header.seq = ++_seq_yawpv;
-            msg.header.stamp = ros::Time::now();
-            msg.header.frame_id = "";
-            msg.point.x = yawpv.x;
-            msg.point.y = yawpv.y;
-            msg.point.z = yawpv.z;
-            _yawpv_prov_pub.publish(msg);
-
-        }else if(t_channel == (int)ros_broadcast_channels::yaw_rate){
-            Vector3D<float> yawratepv = vector3d_msg->getData();
-            geometry_msgs::PointStamped msg;
-            msg.header.seq = ++_seq_yawratepv;
-            msg.header.stamp = ros::Time::now();
-            msg.header.frame_id = "";
-            msg.point.x = yawratepv.x;
-            msg.point.y = yawratepv.y;
-            msg.point.z = yawratepv.z;
-            _yawratepv_prov_pub.publish(msg);
 
         }else if(t_channel == (int)ros_broadcast_channels::roll){
             Vector3D<float> rollpv = vector3d_msg->getData();
             _att.x = rollpv.x;
             roll_received = true;
-            geometry_msgs::PointStamped msg;
-            msg.header.seq = ++_seq_rollpv;
-            msg.header.stamp = ros::Time::now();
-            msg.header.frame_id = "";
-            msg.point.x = rollpv.x;
-            msg.point.y = rollpv.y;
-            msg.point.z = rollpv.z;
-            _rollpv_prov_pub.publish(msg);
+
         }else if(t_channel == (int)ros_broadcast_channels::pitch){
             Vector3D<float> pitchpv = vector3d_msg->getData();
             _att.y = pitchpv.x;
             pitch_received = true;
-            geometry_msgs::PointStamped msg;
-            msg.header.seq = ++_seq_pitchpv;
-            msg.header.stamp = ros::Time::now();
-            msg.header.frame_id = "";
-            msg.point.x = pitchpv.x;
-            msg.point.y = pitchpv.y;
-            msg.point.z = pitchpv.z;
-            _pitchpv_prov_pub.publish(msg);
+            
         }
     }else if(t_msg->getType() == msg_type::VECTORDOUBLE){
         VectorDoubleMsg* vector_double_msg = (VectorDoubleMsg*)t_msg;
@@ -156,10 +92,7 @@ void ROSUnit_BroadcastData::receiveMsgData(DataMessage* t_msg, int t_channel){
         }
     }else if(t_msg->getType() == msg_type::INTEGER){
         IntegerMsg* integer_msg = (IntegerMsg*)t_msg;
-        if(t_channel == ros_broadcast_channels::waypoints){
-            _number_of_waypoints = integer_msg->data; 
-            
-        }else if(t_channel == ros_broadcast_channels::error){
+        if(t_channel == ros_broadcast_channels::error){
             _error_accumulator += integer_msg->data;
             geometry_msgs::PointStamped msg;
             msg.header.seq = ++_seq_xpv;
@@ -177,7 +110,6 @@ void ROSUnit_BroadcastData::receiveMsgData(DataMessage* t_msg, int t_channel){
         msg.header.seq = ++_seq_info;
         msg.header.stamp = ros::Time::now();
         msg.header.frame_id = "";
-        msg.number_of_waypoints = _number_of_waypoints;
         msg.armed = _armed;
         msg.battery_voltage = _voltage;
         _info_prov_pub.publish(msg);
